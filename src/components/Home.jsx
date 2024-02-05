cb5de7672717bef173d9c2d7d685f6053960290a
Niharika Deb <127780989+nihaa222@users.noreply.github.com>
2024-02-05 15:38:35 +0530

    yes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div id="Home" className="max-w-[1300px] mx-auto mt-20 lg:mt-56">
       <div className="flex flex-col lg:flex-row mx-3 lg:w-1/3">
-        <div className="flex  flex-col gap-3 items-center  lg:items-start ">
+        <div className="flex  flex-col gap-3 items-center m-5  lg:items-start ">
           <p className="font-bold">Plan your trip now</p>
           <p className="font-bold leading-[45px] text-center lg:text-start text-[44px] lg:text-3xl">
             Save <span className="text-red-500 ">big </span>with our car rental
